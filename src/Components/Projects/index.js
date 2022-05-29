749308afbd690c86ec5dfa718123e156c141e256
Gonzalo Collares <collares.gonzalo@gmail.com>
2022-05-29 15:51:02 -0300

    TG-05: new branch and app connected

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -1,9 +1,33 @@
 import styles from './projects.module.css';
+import { useEffect, useState } from 'react';
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+  useEffect(async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/projects`);
+      const data = await response.json();
+      setProjects(data.data);
+      // eslint-disable-next-line no-console
+      console.log(data);
+      // eslint-disable-next-line no-console
+      console.log(projects);
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+    }
+  }, []);
+
   return (
     <section className={styles.container}>
       <h2>Projects</h2>
+      <div>
+        {projects.map((project) => (
+          <div key={project._id}>
+            <p>{project.projectName}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
