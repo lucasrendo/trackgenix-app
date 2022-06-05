92d8e3fff01bfeaa54d4d975b2865e3a0f173da6
FedeBrus64 <fedeJbruschi@gmail.com>
2022-06-05 17:32:15 -0300

    TG-34: Modal fully implemented

--- src/Components/Shared/Modal/Modal.jsx
+++ src/Components/Shared/Modal/Modal.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import styles from './modal.module.css'
+import React from 'react';
+import styles from './modal.module.css';
 
-const Modal = ({children, isOpen, handleClose}) =>{
-    if (!isOpen){
-        return null;
-    };
+const Modal = ({ children, isOpen, handleClose }) => {
+  if (!isOpen) {
+    return null;
+  }
 
-    return (
-        <div className={styles.modalOverlay}> 
-            <div className={styles.modalWrapper}>
-                <button onClick={handleClose} className={styles.closeButton}>
-                    X
-                </button>
-                {children}
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.modalOverlay}>
+      <div className={styles.modalWrapper}>
+        <button onClick={handleClose} className={styles.closeButton}>
+          X
+        </button>
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
