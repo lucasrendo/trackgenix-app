1f8a8b9ef3f6c1d494f86d780ff838e3fde173aa
Lucas Rendo <lucasrendo1@gmail.com>
2022-06-07 13:21:13 -0300

    TG-41: delete old components, apply form component into admin and add routes to it

--- src/Components/Admins/index.js
+++ src/Components/Admins/index.js
@@ -1,56 +1,111 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import styles from './admins.module.css';
-import List from './List/List';
-import Form from './Form/index';
+import List from '../Shared/List/List';
+import Button from '../Shared/Button/Button';
+import Loading from '../Shared/Loading/Loading';
 
 const Admins = () => {
   const [admins, setAdmins] = useState([]);
-  const [screen, changeScreen] = useState(false);
-  const [method, setMethod] = useState('POST');
-  const [adminId, setAdminId] = useState('');
+  const [isLoading, setIsLoading] = useState([true]);
+  const serverPath = '/admins';
+  const config = [
+    {
+      title: 'First Name',
+      type: 'text',
+      id: 'firstName',
+      required: true
+    },
+    {
+      title: 'Last Name',
+      type: 'text',
+      id: 'lastName',
+      required: true
+    },
+    {
+      title: 'email',
+      type: 'email',
+      id: 'email',
+      required: true
+    },
+    {
+      title: 'Password',
+      type: 'string',
+      id: 'password',
+      required: true
+    },
+    {
+      title: 'Is Active?',
+      type: 'checkbox',
+      id: 'isActive',
+      required: false
+    }
+  ];
+
+  const headers = [
+    { header: 'First name', key: 'firstName' },
+    { header: 'Last name', key: 'lastName' },
+    { header: 'Email', key: 'email' },
+    { header: 'is Active?', key: 'isActive' }
+  ];
 
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_API_URL}/admins`)
-      .then((response) => response.json())
-      .then((response) => {
-        setAdmins(response.data);
-      });
+    getAdmins();
   }, []);
 
-  const createAdmin = (adm) => {
-    const newAdmin = { ...adm };
-    setAdmins([...admins, newAdmin]);
-  };
-
-  const editAdmin = (id) => {
-    setMethod('PUT');
-    changeScreen(true);
-    setAdminId(id);
+  const getAdmins = async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/admins`);
+      const body = await response.json();
+      setAdmins(body.data);
+      setIsLoading(false);
+    } catch (error) {
+      alert(error);
+    }
   };
 
   const deleteAdmin = (_id) => {
     setAdmins([...admins.filter((admin) => admin._id !== _id)]);
   };
 
-  return (
+  const formatListData = (responseData) => {
+    const data = responseData.map((admin) => {
+      return {
+        firstName: admin.firstName,
+        lastName: admin.lastName,
+        email: admin.email,
+        isActive: admin.isActive.toString()
+      };
+    });
+    return data;
+  };
+
+  return isLoading ? (
+    <>
+      <h2>Admins</h2>
+      <Loading />
+    </>
+  ) : (
     <section className={styles.container}>
       <h2>Admins</h2>
       <div>
-        <button onMouseDown={() => changeScreen(false)}>Admin List</button>
-        <button onMouseDown={() => changeScreen(true)}>Save Admin</button>
+        <Link
+          to={{
+            pathname: '/admins/form',
+            linkData: config,
+            itemData: ''
+          }}
+          className={styles.LinkReset}
+        >
+          <Button classes="block">Create Admin</Button>
+        </Link>
       </div>
-      {screen ? (
-        <Form
-          onAdd={createAdmin}
-          method={method}
-          setMethod={setMethod}
-          adminId={adminId}
-          editAdmin={editAdmin}
-        />
-      ) : (
-        <List list={admins} setList={setAdmins} deleteAdmin={deleteAdmin} editAdmin={editAdmin} />
-      )}
-      ;
+      <List
+        data={formatListData(admins)}
+        headers={headers}
+        resource={serverPath}
+        deleteAdmin={deleteAdmin}
+      />
     </section>
   );
 };
