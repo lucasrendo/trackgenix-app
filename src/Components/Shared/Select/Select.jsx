35b350a4d863adca4b255029f3ae88a5d10e8bee
FedeBrus64 <fedeJbruschi@gmail.com>
2022-06-06 23:14:15 -0300

    TG-34: TextArea component added and styled

--- src/Components/Shared/Select/Select.jsx
+++ src/Components/Shared/Select/Select.jsx
@@ -3,11 +3,11 @@ import styles from './select.module.css'
 
 const Select = ({text, id, value, required}) => {
     return (
-        <div>
-            <label className="">
+        <div className={styles.selectContainer}>
+            <label className={styles.labelSelect}>
                 {text}
             </label>
-            <select className="" id={id} value={value} required={required}>
+            <select className={styles.selectBox} id={id} value={value} required={required}>
             </select>
         </div>
     );
