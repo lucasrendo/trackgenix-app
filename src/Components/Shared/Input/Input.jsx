444e022d703483416355d5e528ba9baa60d195f1
FedeBrus64 <fedeJbruschi@gmail.com>
2022-06-07 12:13:45 -0300

    TG-34: Components improved to receive more props

--- src/Components/Shared/Input/Input.jsx
+++ src/Components/Shared/Input/Input.jsx
@@ -1,17 +1,20 @@
-import React from "react";
-import styles from './input.module.css'
+import React from 'react';
+import styles from './input.module.css';
 
-const Input = ({text, type, id, required, value}) => {
-    return (
-        <div className={styles.inputContainer}>
-            <label className={styles.label}>
-                {text}
-            </label>
-            <input className={styles.input} type={type} id={id} required={required} value={value}>
-            </input>
-        </div>
-    );
+const Input = ({ text, type, id, required, value, onChange }) => {
+  return (
+    <div className={styles.inputContainer}>
+      <label className={styles.labelInput}>{text}</label>
+      <input
+        className={styles.inputBox}
+        type={type}
+        id={id}
+        required={required}
+        value={value}
+        onChange={onChange}
+      ></input>
+    </div>
+  );
 };
 
 export default Input;
-
