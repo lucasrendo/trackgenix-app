8c1f8078f0b5b35c205f1243200ae2ec6e6bac67
FedeBrus64 <fedeJbruschi@gmail.com>
2022-06-09 14:51:51 -0300

    TG-32: Modal corrected

--- src/Components/Shared/Modal/Index.jsx
+++ src/Components/Shared/Modal/Index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './modal.module.css';
+import styles from './index.module.css';
 
 const Modal = ({ children, isOpen, handleClose, isConfirmation, confirmed }) => {
   if (!isOpen) {
