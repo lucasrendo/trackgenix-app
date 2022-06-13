ec7f89abc560eb317198501f4e4cccc362c70eed
Lucas Rendo <lucasrendo1@gmail.com>
2022-06-12 23:02:13 -0300

    TG-53: change actions and thunks files so only thunks return functions

---
+++ src/redux/Task/reducer.js
@@ -0,0 +1,72 @@
+import {
+  GET_TASKS_PENDING,
+  GET_TASKS_FULFILLED,
+  GET_TASKS_FAILED,
+  GET_SINGLE_TASK_PENDING,
+  GET_SINGLE_TASK_FULFILLED,
+  GET_SINGLE_TASK_FAILED,
+  CREATE_TASK_PENDING,
+  CREATE_TASK_FULFILLED,
+  CREATE_TASK_FAILED,
+  UPDATE_TASK_PENDING,
+  UPDATE_TASK_FULFILLED,
+  UPDATE_TASK_FAILED,
+  DELETE_TASK_PENDING,
+  DELETE_TASK_FULFILLED,
+  DELETE_TASK_FAILED
+} from './constants';
+
+const initialState = {
+  isLoading: false,
+  error: '',
+  message: '',
+  task: {},
+  list: []
+};
+
+export const tasksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_SINGLE_TASK_PENDING:
+      return { ...state, isLoading: true };
+    case GET_SINGLE_TASK_FULFILLED:
+      return {
+        ...state,
+        isLoading: false,
+        task: action.payload
+      };
+    case GET_SINGLE_TASK_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+    case CREATE_TASK_PENDING:
+      return { ...state, isLoading: true };
+    case CREATE_TASK_FULFILLED:
+      return {
+        ...state,
+        isLoading: false,
+        task: action.payload
+      };
+    case CREATE_TASK_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+    case UPDATE_TASK_PENDING:
+      return { ...state, isLoading: true };
+    case UPDATE_TASK_FULFILLED:
+      return {
+        ...state,
+        isLoading: false,
+        task: action.payload
+      };
+    case UPDATE_TASK_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+  }
+};
--- src/redux/Task/thunks.js
+++ src/redux/Task/thunks.js
@@ -1,75 +1,113 @@
+import {
+  getTasksPending,
+  getTasksFulfilled,
+  getTasksFailed,
+  getSingleTaskPending,
+  getSingleTaskFulfilled,
+  getSingleTaskFailed,
+  createTaskPending,
+  createTaskFulfilled,
+  createTaskFailed,
+  updateTaskPending,
+  updateTaskFulfilled,
+  updateTaskFailed,
+  deleteTaskPending,
+  deleteTaskFulfilled,
+  deleteTaskFailed
+} from './actions';
+
 const url = `${process.env.REACT_APP_API_URL}/tasks`;
 
-export const getTasksRequest = async () => {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
+export const getTasks = () => {
+  return async (dispatch) => {
+    try {
+      dispatch(getTasksPending());
+      const response = await fetch(url);
+      const data = await response.json();
 
-    return data;
-  } catch (error) {
-    return error;
-  }
+      if (!data.error) dispatch(getTasksFulfilled(data));
+      else dispatch(getTasksFailed(data.message));
+    } catch (error) {
+      dispatch(getTasksFailed(error));
+    }
+  };
 };
 
-export const getSingleTaskRequest = async (id) => {
-  try {
-    const response = await fetch(`${url}/${id}`);
-    const data = await response.json();
+export const getSingleTask = async (id) => {
+  return async (dispatch) => {
+    try {
+      dispatch(getSingleTaskPending());
+      const response = await fetch(`${url}/${id}`);
+      const data = await response.json();
 
-    return data;
-  } catch (error) {
-    return error;
-  }
+      if (!data.error) dispatch(getSingleTaskFulfilled(data));
+      else dispatch(getSingleTaskFailed(data.message));
+    } catch (error) {
+      dispatch(getSingleTaskFailed(error));
+    }
+  };
 };
 
-export const createTaskRequest = async (object) => {
-  try {
-    const requestConfig = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(object)
-    };
+export const createTask = async (object) => {
+  return async (dispatch) => {
+    try {
+      const requestConfig = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(object)
+      };
 
-    const response = await fetch(`${url}`, requestConfig);
-    const data = await response.json();
+      dispatch(createTaskPending());
+      const response = await fetch(`${url}`, requestConfig);
+      const data = await response.json();
 
-    return data;
-  } catch (error) {
-    return error;
-  }
+      if (!data.error) dispatch(createTaskFulfilled(data));
+      else dispatch(createTaskFailed(data.message));
+    } catch (error) {
+      dispatch(createTaskFailed(error));
+    }
+  };
 };
 
-export const updateTaskRequest = async (object, id) => {
-  try {
-    const requestConfig = {
-      method: 'PUT',
-      headers: { 'content-type': 'application/json' },
-      body: JSON.stringify(object)
-    };
+export const updateTask = async (object, id) => {
+  return async (dispatch) => {
+    try {
+      const requestConfig = {
+        method: 'PUT',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(object)
+      };
 
-    const res = await fetch(`${url}/${id}`, requestConfig);
-    const data = await res.json();
+      dispatch(updateTaskPending());
+      const response = await fetch(`${url}/${id}`, requestConfig);
+      const data = await response.json();
 
-    return data;
-  } catch (error) {
-    return error;
-  }
+      if (!data.error) dispatch(updateTaskFulfilled(data));
+      else dispatch(updateTaskFailed(data.message));
+    } catch (error) {
+      dispatch(createTaskFailed(error));
+    }
+  };
 };
 
-export const deleteTaskRequest = async (id) => {
-  try {
-    const requestConfig = {
-      method: 'DELETE',
-      headers: {
-        'Content-type': 'application/json'
-      }
-    };
+export const deleteTask = async (id) => {
+  return async (dispatch) => {
+    try {
+      const requestConfig = {
+        method: 'DELETE',
+        headers: {
+          'Content-type': 'application/json'
+        }
+      };
 
-    const response = await fetch(`${url}/${id}`, requestConfig);
-    const data = await response.json();
+      dispatch(deleteTaskPending());
+      const response = await fetch(`${url}/${id}`, requestConfig);
+      const data = await response.json();
 
-    return data;
-  } catch (error) {
-    return error;
-  }
+      if (!data.error) dispatch(deleteTaskFulfilled(data));
+      else dispatch(deleteTaskFailed(data.message));
+    } catch (error) {
+      dispatch(deleteTaskFailed(error));
+    }
+  };
 };
