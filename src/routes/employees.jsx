b9cdbe6cd0b2037afa183a0466e228058036ad4c
lautaro acosta <lautaroeacosta23@gmail.com>
2022-06-18 10:27:35 -0300

    TG-80: correction in routes

--- src/routes/employees.jsx
+++ src/routes/employees.jsx
@@ -2,7 +2,7 @@ import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import EmployeeLayout from 'Components/Employee/Layout';
 import Loading from 'Components/Shared/Loading';
-// const EmployeeProfile = lazy(() => import('Components/Employee/UserProfile'));
+const EmployeeProfile = lazy(() => import('Components/Employee/UserProfile'));
 
 const employeeRoutes = () => {
   return (
@@ -12,7 +12,7 @@ const employeeRoutes = () => {
           <Switch>
             <Route exact path={'/employee/:id'} />
             <Route exact path={'/employee/projects/:id'} />
-            {/* <Route exact path={'/employee/profile/:id'} component={EmployeeProfile} /> */}
+            <Route exact path={'/employee/profile/:id'} component={EmployeeProfile} />
             <Route exact path={'/employee/workedhours/:id'} />
           </Switch>
         </Suspense>
