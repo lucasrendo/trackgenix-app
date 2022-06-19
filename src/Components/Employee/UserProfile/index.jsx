6fefb06c78af3882ce8200732f2e4b73d44e431a
lautaro acosta <lautaroeacosta23@gmail.com>
2022-06-19 11:45:57 -0300

    TG-80: add validations with joi

--- src/Components/Employee/UserProfile/index.jsx
+++ src/Components/Employee/UserProfile/index.jsx
@@ -3,10 +3,13 @@ import { useParams, useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { resetEmployee, resetMessage } from '../../../redux/employees/actions';
 import { getSingleEmployee, createEmployee } from 'redux/employees/thunks';
+import { useForm } from 'react-hook-form';
+import { joiResolver } from '@hookform/resolvers/joi';
 import Form from 'Components/Shared/Form';
 import Loading from 'Components/Shared/Loading';
 import Modal from 'Components/Shared/Modal/Modal';
 import styles from './index.module.css';
+import Joi from 'joi';
 
 const EmployeeProfile = () => {
   const { id } = useParams();
@@ -23,6 +26,23 @@ const EmployeeProfile = () => {
     id && dispatch(getSingleEmployee(id));
     return () => dispatch(resetEmployee());
   }, []);
+
+  const profileValidate = Joi.object({
+    firstName: Joi.string().label('First Name').min(3).max(10).required(),
+    lastName: Joi.string().label('Last Name').min(3).max(10).required(),
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+      .required('Email is required'),
+    password: Joi.string().label('Password').min(8).required(),
+    repeatPassword: Joi.string().label('Repeat Password').required().valid(Joi.ref('password')),
+    secretWord: Joi.string().label('Secret Word').min(8).required(),
+    repeatSecreteWord: Joi.string()
+      .label('Repeat Secret Word')
+      .required()
+      .valid(Joi.ref('secretWord')),
+    address: Joi.string().label('Address').min(3).max(10).required(),
+    birthDate: Joi.date().label('Birth Date').required()
+  });
 
   const config = [
     {
@@ -44,13 +64,31 @@ const EmployeeProfile = () => {
       required: true
     },
     {
+      header: 'Repeat Email',
+      type: 'email',
+      key: 'email',
+      required: true
+    },
+    {
       header: 'Password',
       type: 'password',
       key: 'password',
       required: true
     },
     {
+      header: 'Repeat Password',
+      type: 'password',
+      key: 'password',
+      required: true
+    },
+    {
       header: 'Secret Word',
+      type: 'password',
+      key: 'password',
+      required: true
+    },
+    {
+      header: 'Repeat Secret Word',
       type: 'password',
       key: 'password',
       required: true
