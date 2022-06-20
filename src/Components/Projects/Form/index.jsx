173bafdc4033e491b2e383a860cc83e71487e386
Lucas Rendo <lucasrendo1@gmail.com>
2022-06-20 20:46:28 -0300

    TG-77: implement react-hook-form and fix minor errors on the code

--- src/Components/Projects/Form/index.jsx
+++ src/Components/Projects/Form/index.jsx
@@ -1,27 +1,70 @@
+import joi from 'joi';
 import React, { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
-import Modal from '../../Shared/Modal/Modal';
-import Form from '../../Shared/Form';
-import styles from './index.module.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { addProject, editProject, getSingleProject } from '../../../redux/projects/thunks';
+import { useForm } from 'react-hook-form';
+import { joiResolver } from '@hookform/resolvers/joi';
+import Input from 'Components/Shared/Input';
+import Select from 'Components/Shared/Select';
+import Button from 'Components/Shared/Button';
 import Loading from '../../Shared/Loading';
-import { resetProject } from '../../../redux/projects/actions';
+import Modal from '../../Shared/Modal/Modal';
+import { resetProject, resetMessage } from '../../../redux/projects/actions';
+import { addProject, editProject, getSingleProject } from '../../../redux/projects/thunks';
 import { getEmployees } from '../../../redux/employees/thunks';
 import { getAdmins } from '../../../redux/admins/thunks';
+import styles from './index.module.css';
 
 function Projects() {
   const { id } = useParams();
   const { goBack } = useHistory();
-  const [inputValues, setInputValues] = useState({});
-  const [showModal, setShowModal] = useState(false);
   const dispatch = useDispatch();
+  const [showModal, setShowModal] = useState(false);
   const project = useSelector((state) => state.projects.project);
   const isLoading = useSelector((state) => state.projects.isLoading);
+  const employeesLoading = useSelector((state) => state.employees.isLoading);
+  const adminsLoading = useSelector((state) => state.admins.isLoading);
   const error = useSelector((state) => state.projects.error);
   const message = useSelector((state) => state.projects.message);
-  const employees = useSelector((state) => state.employees.list);
-  const admins = useSelector((state) => state.admins.list);
+  const employeeList = useSelector((state) => state.employees.list);
+  const adminList = useSelector((state) => state.admins.list);
+
+  const validationSchema = joi.object({
+    projectName: joi.string().min(1).max(20).required().label('Project name'),
+    description: joi.string().min(10).max(140).label('Description'),
+    isActive: joi.boolean(),
+    admin: joi.string().required().label('Admin'),
+    client: joi.string().min(1).max(20).required().label('Client'),
+    startDate: joi.date().required().label('Start Date'),
+    endDate: joi.date().min(joi.ref('startDate')).label('End Date'),
+    employeeId: joi.string().required().label('Employee'),
+    role: joi.string().valid('DEV', 'QA', 'PM', 'TL').required().label('Role'),
+    rate: joi.number().min(0).precision(2).required().label('Rate'),
+    hoursInProject: joi.number().min(0).precision(1).required().label('Hours In Project')
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset
+  } = useForm({
+    mode: 'onBlur',
+    defaultValues: {
+      projectName: '',
+      description: '',
+      startDate: '',
+      endDate: '',
+      admin: '',
+      client: '',
+      employeeId: '',
+      role: '',
+      rate: 0,
+      hoursInProject: 0,
+      isActive: false
+    },
+    resolver: joiResolver(validationSchema)
+  });
 
   useEffect(() => {
     id && dispatch(getSingleProject(id));
@@ -30,100 +73,30 @@ function Projects() {
     return () => dispatch(resetProject());
   }, []);
 
-  const formatDataOptions = () => {
-    let employeesData = [];
-    let adminsData = [];
-    admins.forEach((admin) => {
-      adminsData.push({ id: admin._id, text: `${admin.firstName} ${admin.lastName}` });
+  useEffect(() => {
+    project && reset(project);
+  }, [project]);
+
+  const formatAdmins = () => {
+    return adminList.map((admin) => {
+      return { id: admin._id, text: `${admin.firstName} ${admin.lastName}` };
     });
-    employees.forEach((employee) => {
-      employeesData.push({ id: employee._id, text: `${employee.firstName} ${employee.lastName}` });
-    });
-    const config = [
-      {
-        header: 'Project Name',
-        type: 'text',
-        key: 'projectName',
-        required: true
-      },
-      {
-        header: 'Description',
-        type: 'text',
-        key: 'description',
-        required: false
-      },
-      {
-        header: 'Start Date',
-        type: 'date',
-        key: 'startDate',
-        required: true
-      },
-      {
-        header: 'End Date',
-        type: 'date',
-        key: 'endDate',
-        required: false
-      },
-      {
-        header: 'Admin',
-        type: 'select',
-        key: 'admin',
-        options: adminsData,
-        required: true
-      },
-      {
-        header: 'Client',
-        type: 'text',
-        key: 'client',
-        required: true
-      },
-      {
-        header: 'Employees',
-        type: 'select',
-        key: 'employees',
-        options: employeesData,
-        required: true
-      },
-      {
-        header: 'Role',
-        type: 'text',
-        key: 'role',
-        required: true
-      },
-      {
-        header: 'Rate',
-        type: 'number',
-        key: 'rate',
-        required: true
-      },
-      {
-        header: 'Hours in projects',
-        type: 'number',
-        key: 'hoursInProject',
-        required: true
-      },
-      {
-        header: 'Is active',
-        type: 'checkbox',
-        key: 'isActive',
-        required: false
-      }
-    ];
-    return config;
   };
 
-  const submitHandler = async (e) => {
-    e.preventDefault();
-    if (id) {
-      dispatch(editProject(inputValues, id));
-    } else {
-      dispatch(addProject(inputValues));
-    }
+  const formatEmployees = () => {
+    return employeeList.map((employee) => {
+      return { id: employee._id, text: `${employee.firstName} ${employee.lastName}` };
+    });
+  };
+
+  const submitHandler = (data) => {
+    id ? dispatch(editProject(data, id)) : dispatch(addProject(data));
     setShowModal(true);
   };
 
   const closeHandler = () => {
     setShowModal(false);
+    dispatch(resetMessage());
     if (!error) {
       goBack();
     }
@@ -132,15 +105,76 @@ function Projects() {
   return (
     <section className={styles.container}>
       <h2>Projects</h2>
-      {isLoading ? (
+      {isLoading || employeesLoading || adminsLoading ? (
         <Loading />
       ) : (
-        <Form
-          data={formatDataOptions()}
-          itemData={project}
-          submitHandler={submitHandler}
-          userInput={[inputValues, setInputValues]}
-        />
+        <form className={styles.form} onSubmit={handleSubmit(submitHandler)}>
+          <Input
+            type="text"
+            id="projectName"
+            text="Project Name"
+            error={errors.projectName}
+            register={register}
+          />
+          <Input
+            type="text"
+            id="description"
+            text="Description"
+            error={errors.description}
+            register={register}
+          />
+          <Input
+            type="date"
+            id="startDate"
+            text="Start Date"
+            error={errors.startDate}
+            register={register}
+          />
+          <Input
+            type="date"
+            id="endDate"
+            text="End Date"
+            error={errors.endDate}
+            register={register}
+          />
+          <Select
+            text="Admin"
+            id="admin"
+            options={formatAdmins()}
+            error={errors.admin}
+            register={register}
+          />
+          <Input type="text" id="client" text="Client" error={errors.client} register={register} />
+          <Select
+            text="Employees"
+            id="employeeId"
+            options={formatEmployees()}
+            error={errors.employeeId}
+            register={register}
+          />
+          <Input type="text" id="role" text="Role" error={errors.role} register={register} />
+          <Input type="number" id="rate" text="Rate" error={errors.rate} register={register} />
+          <Input
+            type="number"
+            id="hoursInProject"
+            text="Hours in project"
+            error={errors.hoursInProject}
+            register={register}
+          />
+          <Input
+            type="checkbox"
+            id="isActive"
+            text="is Active?"
+            error={errors.isActive?.message}
+            register={register}
+          />
+          <div className={styles.btnsContainer}>
+            <Button classes={'red'} onClick={() => goBack()}>
+              Back
+            </Button>
+            <Button>Save</Button>
+          </div>
+        </form>
       )}
       <Modal handleClose={() => closeHandler()} isOpen={showModal} isConfirmation={false}>
         <h2>{message}</h2>
