84704c2893fc17a565c1cfeb89878f7c18d907ef
FranGutierrez <55754815+FranGutierrez@users.noreply.github.com>
2022-06-21 10:34:20 -0300

    TG-76: MyProjects screen working correctly

--- src/Components/Employee/MyProjects/index.jsx
+++ src/Components/Employee/MyProjects/index.jsx
@@ -1,24 +1,16 @@
-import { useState, useEffect } from 'react';
-import { useParams, useHistory, Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { getSingleEmployee, getEmployees } from 'redux/employees/thunks';
+import { getSingleEmployee } from 'redux/employees/thunks';
 import List from 'Components/Shared/List';
 import Loading from 'Components/Shared/Loading';
-import Modal from 'Components/Shared/Modal/Modal';
 import styles from './index.module.css';
 
 const EmployeeProjects = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
   const employee = useSelector((state) => state.employees.employee);
-  // const [employee, setEmployee] = useState();
-  // const list = useSelector((state) => state.employees.list);
   const isLoading = useSelector((state) => state.employees.isLoading);
-  const message = useSelector((state) => state.employees.message);
-  const error = useSelector((state) => state.employees.error);
-  const showModal = useSelector((state) => state.employees.showModal);
-  const [projects, setProjects] = useState([]);
-  const [confirmation, setConfirmation] = useState(true);
   const headers = [
     { header: 'Project Name', key: 'projectName' },
     { header: 'Client', key: 'client' },
@@ -28,28 +20,33 @@ const EmployeeProjects = () => {
 
   useEffect(() => {
     id && dispatch(getSingleEmployee(id));
-    //dispatch(getEmployees());
   }, []);
 
-  // const filterEmployee = (list) => {
-  //   const data = list.filter((employees) => {
-  //     return employees._id === id;
-  //   });
-  //   setEmployee(data[0]);
-  //   console.log(employee.assignedProjects);
-  //   return employee.assignedProjects;
-  // };
-
-  const getEmployeeProjects = () => {
-    setProjects(employee.assignedProjects ? employee.assignedProjects : []);
-    console.log(projects);
-    return projects;
+  const formatListData = (responseData) => {
+    const data = responseData.map((project) => {
+      return {
+        id: project._id,
+        startDate: project.startDate.slice(0, 10),
+        endDate: project.endDate.slice(0, 10),
+        projectName: project.projectName,
+        client: project.client
+      };
+    });
+    return data;
   };
 
   return (
     <section className={styles.container}>
       <h2>My Projects</h2>
-      {isLoading ? <Loading /> : <List data={[]} headers={headers} showButtons={false} />}
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <List
+          data={formatListData(employee?.assignedProjects || [])}
+          headers={headers}
+          showButtons={false}
+        />
+      )}
     </section>
   );
 };
