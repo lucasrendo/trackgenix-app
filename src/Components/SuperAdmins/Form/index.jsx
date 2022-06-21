a18f117d7eb74b7f6404a1a7579c2edd8f6f9ab7
FedeBrus64 <fedeJbruschi@gmail.com>
2022-06-21 04:20:51 -0300

    TG-75: Validations and styles made

--- src/Components/SuperAdmins/Form/index.jsx
+++ src/Components/SuperAdmins/Form/index.jsx
@@ -12,6 +12,48 @@ import Form from '../../Shared/Form';
 import Modal from '../../Shared/Modal/Modal';
 import styles from './super-admins.module.css';
 import { appendErrors, useForm } from 'react-hook-form';
+import Input from 'Components/Shared/Input';
+import Button from 'Components/Shared/Button';
+import Joi from 'joi';
+import { joiResolver } from '@hookform/resolvers/joi';
+
+const superAdminValidate = Joi.object({
+  firstName: Joi.string()
+    .pattern(/^[a-zA-Z]+$/)
+    .label('First Name')
+    .min(3)
+    .max(10)
+    .required()
+    .messages({
+      'string.pattern.base': `First Name must only have letters`,
+      'string.empty': `First Name cannot be empty`,
+      'string.max': `First name cannot have more than 10 characters`,
+      'string.min': `First name must have at least 3 characters`
+    }),
+  lastName: Joi.string()
+    .pattern(/^[a-zA-Z]+$/)
+    .label('Last Name')
+    .min(3)
+    .max(10)
+    .required()
+    .messages({
+      'string.pattern.base': `Last Name must only have letters`,
+      'string.empty': `Last Name cannot be empty`,
+      'string.max': `Last name cannot have more than 10 characters`,
+      'string.min': `Last name must have at least 3 characters`
+    }),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+    .required()
+    .messages({
+      'string.empty': `Email cannot be empty`
+    }),
+  password: Joi.string().label('Password').min(8).required().messages({
+    'string.empty': `Password cannot be empty`,
+    'string.min': `Password must have at least 8 alphanumeric characters`
+  }),
+  isActive: Joi.boolean()
+});
 
 function SuperAdminsForm() {
   const { id } = useParams();
@@ -26,10 +68,11 @@ function SuperAdminsForm() {
   const {
     handleSubmit,
     register,
-    formState: {
-      errors
-    }
+    formState: { errors },
+    reset
   } = useForm({
+    reValidateMode: 'onChange',
+    resolver: joiResolver(superAdminValidate),
     mode: 'onchange'
   });
   // const resource = '/super-admin';
@@ -39,38 +82,42 @@ function SuperAdminsForm() {
     return () => dispatch(resetSuperAdmin());
   }, []);
 
-  const config = [
-    {
-      header: 'First Name',
-      type: 'text',
-      key: 'firstName',
-      required: true
-    },
-    {
-      header: 'Last Name',
-      type: 'text',
-      key: 'lastName',
-      required: true
-    },
-    {
-      header: 'Email',
-      type: 'email',
-      key: 'email',
-      required: true
-    },
-    {
-      header: 'Password',
-      type: 'password',
-      key: 'password',
-      required: true
-    },
-    {
-      header: 'Is active',
-      type: 'checkbox',
-      key: 'isActive',
-      required: false
-    }
-  ];
+  useEffect(() => {
+    reset(superAdmin);
+  }, [superAdmin]);
+
+  // const config = [
+  //   {
+  //     header: 'First Name',
+  //     type: 'text',
+  //     key: 'firstName',
+  //     required: true
+  //   },
+  //   {
+  //     header: 'Last Name',
+  //     type: 'text',
+  //     key: 'lastName',
+  //     required: true
+  //   },
+  //   {
+  //     header: 'Email',
+  //     type: 'email',
+  //     key: 'email',
+  //     required: true
+  //   },
+  //   {
+  //     header: 'Password',
+  //     type: 'password',
+  //     key: 'password',
+  //     required: true
+  //   },
+  //   {
+  //     header: 'Is active',
+  //     type: 'checkbox',
+  //     key: 'isActive',
+  //     required: false
+  //   }
+  // ];
 
   const closeHandler = () => {
     setModalMessage(false);
@@ -81,63 +128,90 @@ function SuperAdminsForm() {
   };
 
   // === Handle submit data and method === //
-  const submitHandler = (e) => {
-    e.preventDefault();
-    id ? dispatch(editSuperAdmins(inputValues, id)) : dispatch(createSuperAdmins(inputValues));
+  const submitHandler = async (data) => {
+    id ? dispatch(editSuperAdmins(data, id)) : dispatch(createSuperAdmins(data));
     setModalMessage(true);
   };
+
+  // return (
+  //   <section className={styles.container}>
+  //     <h2>Super Admins</h2>
+  //     {isLoading ? (
+  //       <Loading />
+  //     ) : (
+  //       <Form
+  //         data={config}
+  //         itemData={superAdmin}
+  //         submitHandler={submitHandler}
+  //         userInput={[inputValues, setInputValues]}
+  //       />
+  //     )}
+  //     <Modal handleClose={() => closeHandler()} isOpen={modalMessage} isConfirmation={false}>
+  //       <h2>{message}</h2>
+  //     </Modal>
+  //   </section>
+  // );
 
   return (
     <section className={styles.container}>
       <h2>Super Admins</h2>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <Form
-          data={config}
-          itemData={superAdmin}
-          submitHandler={submitHandler}
-          userInput={[inputValues, setInputValues]}
-        />
-      )}
-      <Modal handleClose={() => closeHandler()} isOpen={modalMessage} isConfirmation={false}>
-        <h2>{message}</h2>
-      </Modal>
-    </section>
-  );
-  
-  return (
-    <section className={styles.container}>
-      <h2>Super Admins</h2>
-      <form onSubmit={handleSubmit(submitHandler)}>
+      <form className={styles.form} onSubmit={handleSubmit(submitHandler)}>
         <div className={styles.textContainer}>
-          <label htmlFor='firstName'>First Name</label>
-          <input type='text' {...register('firstName', {required: {value: true, message: ''} })} />
-          {errors.firstName && <p className='asdasd'>{errors.type}</p>}
-        </div>        
-        
-        <div className={styles.textContainer}>
-          <label htmlFor='lastName'>Last Name</label>
-          <input type='text' {...register('lastName', {required: {value: true, message: ''} })} />
-          {errors.lastName && <p className='asdasd'>{errors.type}</p>}
+          <Input
+            id={'firstName'}
+            register={register}
+            text={'First Name'}
+            type={'text'}
+            error={errors.firstName}
+          />
+          {/* {errors.firstName && <p className={styles.errorMessage}>{errors.firstName.message}</p>} */}
         </div>
-        
+
         <div className={styles.textContainer}>
-          <label htmlFor='email'>E-mail</label>
-          <input type='email' {...register('email', {required: {value: true, message: ''} })} />
-          {errors.email && <p className='asdasd'>{errors.type}</p>}
+          <Input
+            id={'lastName'}
+            text={'Last Name'}
+            type={'text'}
+            register={register}
+            error={errors.lastName}
+          />
         </div>
-        
+
         <div className={styles.textContainer}>
-          <label htmlFor='password'>Password</label>
-          <input type='password' {...register('password', {required: {value: true, message: ''} })} />
-          {errors.password && <p className='asdasd'>{errors.type}</p>}
+          <Input
+            id={'email'}
+            text={'Email'}
+            type={'email'}
+            register={register}
+            error={errors.email}
+          />
         </div>
-        
+
+        <div className={styles.textContainer}>
+          <Input
+            id={'password'}
+            text={'Password'}
+            type={'password'}
+            register={register}
+            error={errors.password}
+          />
+        </div>
+
         <div className={styles.checkContainer}>
-          <label htmlFor='isActive'>Is active?</label>
-          <input type='checkbox' {...register('isActive', {required: {value: true, message: ''} })} />
-          {errors.isActive && <p className='asdasd'>{errors.type}</p>}
+          <Input
+            id={'isActive'}
+            type={'checkbox'}
+            text={'Is Active?'}
+            required={false}
+            register={register}
+            error={errors.isActive}
+          />
+        </div>
+        <div className={styles.buttonContainer}>
+          <Button>Okay</Button>
+          <Button classes={'red'} onClick={() => goBack()}>
+            Cancel
+          </Button>
         </div>
       </form>
       <Modal handleClose={() => closeHandler()} isOpen={modalMessage} isConfirmation={false}>
