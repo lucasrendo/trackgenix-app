e7314b9202a01affe0f7dc0bc09a68001d0985f4
Nicolas Micheletti <nicolasamicheletti@gmail.com>
2022-06-23 09:34:07 -0300

    TG-74: PR comment correction

--- src/Components/Employee/Home/index.jsx
+++ src/Components/Employee/Home/index.jsx
@@ -3,14 +3,11 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 function Home() {
-  const employee = useSelector((state) => state.employees.employee);
   return (
     <section className={styles.container}>
       <h2>
         <p>Welcome,</p>
         <p>Jorge Pérez!</p>
-        {/*{employee.firstName}&nbsp;
-        {employee.lastName}*/}
       </h2>
     </section>
   );
