5a5d9bad0788a0090e76e0d72ec152325ddda501
FranGutierrez <55754815+FranGutierrez@users.noreply.github.com>
2022-07-06 14:43:50 -0300

    TG-97: Open modal with form inside & changed some styles

--- src/Components/Employee/WorkedHours/index.jsx
+++ src/Components/Employee/WorkedHours/index.jsx
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Modal from 'Components/Shared/Modal/Modal';
 import Loading from 'Components/Shared/Loading';
 import styles from './index.module.css';
 import Button from 'Components/Shared/Button';
+import Input from 'Components/Shared/Input';
+import Select from 'Components/Shared/Select';
 import { getTimesheetsByEmployee } from 'redux/employee/thunks';
 import { getSingleEmployee } from 'redux/employees/thunks';
+import { getTasks } from 'redux/tasks/thunks';
+import { getSingleProject } from 'redux/projects/thunks';
+import { addTimesheet, editTimesheet, getSingleTimesheet } from 'redux/timesheets/thunks';
+import { resetMessage, resetTimesheet } from 'redux/timesheets/actions';
 import { set, useForm } from 'react-hook-form';
-import { resetMessage } from 'redux/employees/actions.js';
 import Joi from 'joi';
 import { joiResolver } from '@hookform/resolvers/joi';
-import Input from 'Components/Shared/Input';
-import { Link } from 'react-router-dom';
 import {
   endOfWeekWithOptions,
   startOfWeekWithOptions,
@@ -20,7 +24,52 @@ import {
   sub,
   eachDayOfInterval
 } from 'date-fns/esm/fp';
-import { isWithinInterval } from 'date-fns';
+
+const timeSheetValidate = Joi.object({
+  workedHours: Joi.number().required().label('Worked Hours').messages({
+    'string.empty': `Worked Hours cannot be empty`
+  }),
+
+  date: Joi.date().required().min('2022-01-01').label('Date').messages({
+    'string.empty': `Date cannot be empty`,
+    'date.min': `Date cannot be earlier than 2022/01/01`
+  }),
+
+  role: Joi.string()
+    .pattern(/^[a-zA-Z]+$/)
+    .label('Role')
+    .valid('DEV', 'QA', 'PM', 'TL')
+    .insensitive()
+    .min(2)
+    .max(3)
+    .required()
+    .messages({
+      'string.pattern.base': `Role must only have letters`,
+      'string.empty': `Role cannot be empty`,
+      'string.max': `Role cannot have more than 3 characters`,
+      'string.min': `Role must have at least 2 characters`
+    }),
+
+  rate: Joi.number().required().label('Rate').messages({
+    'string.empty': `Rate cannot be empty`
+  }),
+
+  project: Joi.string().label('Project').required().messages({
+    'string.empty': `Project cannot be an empty field`
+  }),
+
+  employee: Joi.string().label('Employee').required().messages({
+    'string.empty': `Employee cannot be an empty field`
+  }),
+
+  task: Joi.string().label('Task').messages({
+    'string.empty': `Task cannot be an empty field`
+  }),
+
+  description: Joi.string().label('Description').min(0).messages({
+    'string.empty': `Description cannot be an empty field`
+  })
+});
 
 const HoursForm = () => {
   const id = '62b1122165165c996de858ec';
@@ -31,12 +80,30 @@ const HoursForm = () => {
   const [listData, setListData] = useState([]);
   const [daysOfWeek, setDaysofWeek] = useState([]);
   const [totalHours, setTotalHours] = useState(0);
-  const [showModal, setShowModal] = useState(true);
+  const [showModal, setShowModal] = useState(false);
   const dispatch = useDispatch();
   const timesheetsList = useSelector((state) => state.employeeTimesheets.list);
+  const timesheet = useSelector((state) => state.timesheet.timesheet);
+  const tasks = useSelector((state) => state.tasks.list);
   const isLoading = useSelector((state) => state.timesheet.isLoading);
+  const error = useSelector((state) => state.timesheet.error);
   const employee = useSelector((state) => state.employees.employee);
-  const message = useSelector((state) => state.employees.message);
+  const message = useSelector((state) => state.timesheet.message);
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    reset
+  } = useForm({
+    mode: 'onBlur',
+    resolver: joiResolver(timeSheetValidate),
+    defaultValues: {
+      task: '',
+      rate: '',
+      workedHours: '',
+      description: ''
+    }
+  });
 
   useEffect(() => {
     if (id) {
@@ -44,11 +111,16 @@ const HoursForm = () => {
       dispatch(getTimesheetsByEmployee(id));
       getCurrentWeek(todayDate);
     }
+    dispatch(getTasks());
   }, []);
 
   useEffect(() => {
     formatListData(employee?.assignedProjects || [], timesheetsList, daysOfWeek);
   }, [week]);
+
+  useEffect(() => {
+    reset(timesheet);
+  }, [timesheet]);
 
   const formatListData = (projects, filteredTimesheets, daysOfWeek) => {
     const formatedWeek = formatDaysOfWeek(daysOfWeek);
@@ -161,25 +233,48 @@ const HoursForm = () => {
     return formatedWeek;
   };
 
+  const formatTasks = () => {
+    return tasks.map((task) => {
+      return { id: task._id, text: task.title };
+    });
+  };
+
   const headers = [
-    { header: 'Project Name', key: 'projectName' },
-    { header: 'Role', key: 'role' },
-    { header: 'Monday', key: 'monday' },
-    { header: 'Tuesday', key: 'tuesday' },
-    { header: 'Wednesday', key: 'wednesday' },
-    { header: 'Thursday', key: 'thursday' },
-    { header: 'Friday', key: 'friday' },
-    { header: 'Saturday', key: 'saturday' },
-    { header: 'Sunday', key: 'sunday' },
-    { header: 'Total Hours', key: 'totalHours' }
+    { header: 'Project Name', key: 'projectName', style: false },
+    { header: 'Role', key: 'role', style: false },
+    { header: 'Monday', key: 'monday', style: true },
+    { header: 'Tuesday', key: 'tuesday', style: true },
+    { header: 'Wednesday', key: 'wednesday', style: true },
+    { header: 'Thursday', key: 'thursday', style: true },
+    { header: 'Friday', key: 'friday', style: true },
+    { header: 'Saturday', key: 'saturday', style: true },
+    { header: 'Sunday', key: 'sunday', style: true },
+    { header: 'Total Hours', key: 'totalHours', style: false }
   ];
+
+  const closeHandler = () => {
+    setShowModal(false);
+    dispatch(resetMessage());
+    // if (!error) {
+    //   goBack();
+    // }
+  };
+
+  const submitHandler = (data) => {
+    if (id) {
+      dispatch(editTimesheet(data, id));
+    } else {
+      dispatch(addTimesheet(data));
+    }
+    setShowModal(true);
+  };
 
   return (
     <section className={styles.container}>
       <h2>Worked Hours</h2>
       <div className={styles.topContainer}>
         <Button onClick={() => prevWeek(startWeekDay, endWeekDay)}>{'<'}</Button>
-        <p>
+        <p className={styles.weekText}>
           {week?.startDate} - {week?.endDate}
         </p>
         <Button onClick={() => nextWeek(startWeekDay, endWeekDay)}>{'>'}</Button>
@@ -206,7 +301,11 @@ const HoursForm = () => {
                   <tr key={row.id} className={styles.rows}>
                     {headers.map((header, index) => {
                       return (
-                        <td key={index} className={styles.td}>
+                        <td
+                          key={index}
+                          className={header.style ? styles.timesheetTd : styles.td}
+                          onClick={() => setShowModal(true)}
+                        >
                           {row[header.key]}
                         </td>
                       );
@@ -217,66 +316,46 @@ const HoursForm = () => {
             </tbody>
           </table>
           <h2 className={styles.totalText}>Total: {totalHours}</h2>
-          {/* <form onSubmit={handleSubmit(submitHandler)} className={styles.form}>
-            <div>
-              <h4>{formatData(timesheetsList)[0]?.projectName || ''}</h4>
-            </div>
-            <input
-              type={'number'}
-              error={errors.monday}
-              onChange={handleInput}
-              name="one"
-              value={number.one}
-            />
-            <input
-              type={'number'}
-              error={errors.tuesday}
-              onChange={handleInput}
-              name="two"
-              value={number.two}
-            />
-            <input
-              type={'number'}
-              error={errors.wednesday}
-              onChange={handleInput}
-              name="three"
-              value={number.three}
-            />
-            <input
-              type={'number'}
-              error={errors.thursday}
-              onChange={handleInput}
-              name="four"
-              value={number.four}
-            />
-            <input
-              type={'number'}
-              error={errors.friday}
-              onChange={handleInput}
-              name="five"
-              value={number.five}
-            />
-            <input
-              type={'number'}
-              error={errors.saturday}
-              onChange={handleInput}
-              name="six"
-              value={number.six}
-            />
-            <input
-              type={'number'}
-              error={errors.sunday}
-              onChange={handleInput}
-              name="seven"
-              value={number.seven}
-            />
-            <input readOnly value={add} />
-          </form> */}
         </>
       )}
-      {/* <Modal isOpen={showModal} isConfirmation={false} handleClose={() => closeHandler()}>
-        <h2>{message}</h2>
-      </Modal> */}
+      {
+        <Modal isOpen={showModal} isConfirmation={true} handleClose={() => closeHandler()}>
+          <h2 className={styles.modalText}>Timesheet</h2>
+          <form className={styles.form} onSubmit={handleSubmit(submitHandler)}>
+            <Select
+              id={'task'}
+              text={'Task'}
+              options={formatTasks()}
+              register={register}
+              error={errors.task}
+            />
+
+            <Input
+              id={'description'}
+              type={'text'}
+              text={'Description'}
+              required={false}
+              register={register}
+              error={errors.description}
+            />
+            <Input
+              id={'workedHours'}
+              text={'Worked Hours'}
+              type={'number'}
+              register={register}
+              error={errors.workedHours}
+            />
+
+            <Input
+              id={'rate'}
+              text={'Rate'}
+              type={'number'}
+              register={register}
+              error={errors.rate}
+            />
+          </form>
+        </Modal>
+      }
     </section>
   );
 };
